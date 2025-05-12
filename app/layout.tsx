867faa618a916dba937096f04c9e11a376364b8a
Ahmad Hassan <a03008164732@gmail.com>
2025-05-12 22:25:58 +0500

    hamburger fixed and resposivness fixed

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning className="scroll-smooth">
       <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </head>
       <body className={`${inter.variable} ${poppins.variable} font-sans`}>
         <ThemeProvider
